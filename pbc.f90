c044942094dc9ddb0f06354594ae577f3af5bbd7
vegalorena24 <vegalorena24@gmail.com>
2017-03-07 22:19:49 +0100

    Update pbc.f90

--- pbc.f90
+++ pbc.f90
@@ -11,7 +11,7 @@ implicit none
 integer::dimnsion,N,i !N=Number of part. 
 real:: BoxSize
 real,dimension(N,dimnsion):: pos !positions
-do i=N
+do i=1,N
  pos(i,:)=pos(i,:)-BoxSize*nint(pos(i,:)/BoxSize) !periodic conditions
 end do
 end subroutine Refold_Positions
