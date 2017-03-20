3e23df0251231983af2aecd3fb3e34c4a26b6757
Oscar Fajardo Fontiveros <oscarcapote@hotmail.es>
2017-03-20 11:32:12 +0100

    Exemple sumar paralel i actualitzacio analisis

---
+++ sumador.f90
@@ -0,0 +1,31 @@
+program hw
+
+include 'mpif.h'
+integer :: comm,rank,numproc,ierror,MASTER
+
+integer(8),dimension(101) :: v
+integer(8) :: suma_part
+integer(8) :: suma_total
+integer(8) :: interval_index
+MASTER = 0
+
+call MPI_INIT(ierror)
+call MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierror)
+call MPI_COMM_Size(MPI_COMM_WORLD,numproc,ierror)
+v = (/ (i , i=1,101) /)
+
+interval_index = size(v)/numproc
+if (rank/=numproc-1)then
+    suma_part = sum(v((rank)*interval_index+1:(rank+1)*interval_index))
+else
+    suma_part = sum(v((rank)*interval_index+1:))
+endif
+call MPI_REDUCE(suma_part,suma_total,1,MPI_INTEGER,MPI_SUM,MASTER,MPI_COMM_WORLD,ierror)
+print*,'Hola mon de processador',rank,'de',numproc,'suma',suma_part,'interval',v((rank+1)*interval_index),v((rank+2)*interval_index)
+
+if(rank==MASTER)then
+print*,'MASTER diu, resultat:',suma_total
+endif
+
+call MPI_FINALIZE(ierror)
+end program
