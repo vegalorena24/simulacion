7918e7d1dfef4e7bc639eccab50730fe063015ee
vegalorena24 <vegalorena24@gmail.com>
2017-03-27 19:40:06 +0200

    LVD: Periodic Boundary conditions en serie

--- pbc.f90
+++ pbc.f90
@@ -5,9 +5,8 @@
 !                                                          Lorena Vega Domínguez
 !********************************************************************************
 
-module pbc
-implicit none
-contains
+
+
 subroutine Refold_Positions(pos,N,dimnsion,BoxSize)
 implicit none
 integer::dimnsion,N,i !N=Number of part. 
@@ -17,4 +16,4 @@ do i=1,N
  pos(i,:)=pos(i,:)-BoxSize*nint(pos(i,:)/BoxSize) !get the part inside the box
 end do
 end subroutine Refold_Positions
-end module
+
