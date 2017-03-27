eb9680db6b336413666334c9626c49a1dcfdf1ef
xmaruganf <xmaruganf@gmail.com>
2017-03-27 15:25:36 +0200

    main_md_alberto+xavi.f90

--- MAIN_DEF/main_md_alberto.f90
+++ MAIN_DEF/main_md_alberto.f90
@@ -72,10 +72,15 @@ open(unit=124,file='restart.rst',status='unknown',action='write')
 
 do step=1,Nsteps
 
+ call IntegrationVerletPositions(positions,velocities,accel,deltat,N,mass,dimnsion,BoxSize)
+ call IntegrationVerletVelocities(velocities,accel,deltat,N,mass)
+
  call forces(positions,BoxSize,ini,fin,accel)
 
- call EulerPositions(positions,velocities,accel,N,dimnsion,BoxSize,mass,deltat)
- call EulerVelocities(positions,velocities,accel,N,dimnsion,BoxSize,mass,deltat)
+ call IntegrationVerletVelocities(velocities,accel,deltat,N,mass)
+ 
+ !call EulerPositions(positions,velocities,accel,N,dimnsion,BoxSize,mass,deltat)
+ !call EulerVelocities(positions,velocities,accel,N,dimnsion,BoxSize,mass,deltat)
 
  if ( rank == MASTER ) then
      print *, '******************************************'
@@ -248,6 +253,115 @@ contains
     end_time=MPI_Wtime()
 
     end subroutine Refold_Positions
+! ======================================================================================
+
+subroutine IntegrationVerletPositions(pos,vel,acc,deltat,npart,mass,dim,lenght)
+
+!include "mpif.h"
+!use paralelizar
+
+real*8,dimension(npart,3),intent(inout)::pos
+real*8,dimension(npart,3),intent(in)::acc,vel
+real*8,intent(in)::deltat,mass,lenght
+integer::npart,dim,MASTER=0,iproc,partxproc,request
+integer::i,ip
+double precision:: start_time, lapso_time,end_time
+
+! START CALCULATION
+start_time=MPI_Wtime()
+! Each worker computes a pice of translation of coordinates
+do i=ini(rank),fin(rank)
+   pos(i,:) = pos(i,:) + deltat*vel(i,:) + 0.5*(deltat**2)*acc(i,:)/mass      ! r(t+dt)
+end do
+lapso_time=MPI_Wtime()
+! END CALCULATION
+
+! sending work of WORKER(i) to MASTER
+if (rank /= MASTER) then
+        call MPI_ISEND(pos(ini(rank):fin(rank),:),3*(fin(rank)-ini(rank)+1),&
+MPI_DOUBLE_PRECISION,MASTER,1,MPI_COMM_WORLD,request,ierror)
+end if
+! Waiting all WORKERS
+call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+
+! MASTER receive and merge
+if (rank == MASTER) then
+  do iproc=1,numproc-1
+    call MPI_RECV(pos(ini(iproc):fin(iproc),:),3*(fin(iproc)-ini(iproc)+1),&
+MPI_DOUBLE_PRECISION,iproc,1,MPI_COMM_WORLD,request,ierror)
+  end do
+
+!!! UPDATE the coordinations of MASTER to WORKERS
+  do iproc=1,numproc-1
+    call MPI_ISEND(pos(:,:),3*npart,MPI_REAL8,iproc,1,MPI_COMM_WORLD,request,ierror)
+  end do
+end if
+
+! Sincronize all processors
+call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+
+! all WORKERS receive coordinates
+if (rank /= MASTER) then
+    call MPI_RECV(pos(:,:),3*npart,MPI_REAL8,MASTER,1,MPI_COMM_WORLD,request,ierror)
+end if
+end_time=MPI_Wtime()
+
+!Condiciones periódicas de contorno
+call Refold_Positions(pos,npart,dim,lenght)
+
+end subroutine IntegrationVerletPositions
+
+! ======================================================================================
+subroutine IntegrationVerletVelocities(vel,acc,deltat,npart,mass)
+
+!include "mpif.h"
+!use paralelizar
+
+real*8,dimension(npart,3),intent(inout)::vel
+real*8,dimension(npart,3),intent(in)::acc
+real*8,intent(in)::deltat,mass
+integer::npart,MASTER=0,iproc,partxproc,request
+double precision:: start_time, lapso_time,end_time
+
+! START CALCULATION
+start_time=MPI_Wtime()
+do i=ini(rank),fin(rank)
+   vel(i,:) = vel(i,:) + 0.5*deltat*acc(i,:)/mass  ! v(t+dt/2)
+end do
+lapso_time=MPI_Wtime()
+! END CALCULATION
+
+! sending work of WORKER(i) to MASTER
+if (rank /= MASTER) then
+        call MPI_ISEND(vel(ini(rank):fin(rank),:),3*(fin(rank)-ini(rank)+1),&
+MPI_DOUBLE_PRECISION,MASTER,1,MPI_COMM_WORLD,request,ierror)
+end if
+! Waiting all WORKERS
+call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+
+! MASTER receive and merge
+if (rank == MASTER) then
+  do iproc=1,numproc-1
+    call MPI_RECV(vel(ini(iproc):fin(iproc),:),3*(fin(iproc)-ini(iproc)+1),&
+MPI_DOUBLE_PRECISION,iproc,1,MPI_COMM_WORLD,request,ierror)
+  end do
+
+!!! UPDATE the coordinations of MASTER to WORKERS
+  do iproc=1,numproc-1
+    call MPI_ISEND(vel(:,:),3*npart,MPI_REAL8,iproc,1,MPI_COMM_WORLD,request,ierror)
+  end do
+end if
+
+! Sincronize all processors
+call MPI_BARRIER(MPI_COMM_WORLD, ierror)
+
+! all WORKERS receive coordinates
+if (rank /= MASTER) then
+    call MPI_RECV(vel(:,:),3*npart,MPI_REAL8,MASTER,1,MPI_COMM_WORLD,request,ierror)
+end if
+end_time=MPI_Wtime()
+
+end subroutine IntegrationVerletVelocities
 ! ======================================================================================
 
 
