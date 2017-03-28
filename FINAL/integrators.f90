ed8086a06ae401dd1e640310634e6c698fe13da2
SergiRB <sergi.rb94@gmail.com>
2017-03-28 23:15:59 +0200

    Integrador de Euler (pos y vel) y PBC

--- FINAL/integrators.f90
+++ FINAL/integrators.f90
@@ -4,159 +4,167 @@ use paralelizar
 
 contains
 
+! ======= EULER POSITIONS ==============================================================================
+subroutine EulerPositions(pos,vel,forces,N,dimnsion,BoxSize,mass,deltat,ini,fin)
 
-    ! ======= EULER POSITIONS ==============================================================================
-        subroutine EulerPositions(pos,vel,forces,N,dimnsion,BoxSize,mass,deltat,ini,fin)
+    integer, intent(in)                      ::  dimnsion, N
+    integer, dimension(0:), intent(in)       ::  ini, fin
+    real*8, intent(in)                       ::  BoxSize, deltat, mass
+    real*8, dimension(:,:), intent(inout)    ::  pos
+    real*8, dimension(:,:), intent(in)       ::  vel, forces
+    double precision                         ::  start_time, lapso_time, end_time
 
-        integer                               :: dimnsion,N,i,request,MASTER=0,iproc,partxproc
-        integer, dimension(0:), intent(in)    :: ini, fin
-        real*8                                :: BoxSize, deltat, mass
-        real*8, dimension(:,:)                :: pos, vel, forces
-        double precision                      :: start_time, lapso_time,end_time
+    ! Update of the positions using Euler
+    start_time=MPI_Wtime()
+    do i=ini(rank),fin(rank)
+        pos(i,:) = pos(i,:) + ( vel(i,:) + deltat * forces(i,:) / mass ) * deltat
+    end do
 
-        ! Update of the positions using Euler
-        start_time=MPI_Wtime()
-        do i=ini(rank),fin(rank)
-         pos(i,:) = pos(i,:) + ( vel(i,:) + deltat*forces(i,:)/mass ) * deltat
-        end do
+    ! Send the positions from workers to Master
+    if (rank /= MASTER) then
+    call MPI_ISEND(pos(ini(rank):fin(rank),:),3*(fin(rank)-ini(rank)+1),&
+                         MPI_REAL8,MASTER,1,MPI_COMM_WORLD,request,ierror)
+    end if
 
-        ! Send the positions from workers to Master
-        if (rank /= MASTER) then
-        call MPI_ISEND(pos(ini(rank):fin(rank),:),3*(fin(rank)-ini(rank)+1),MPI_REAL8,MASTER,1,MPI_COMM_WORLD,request,ierror)
-        end if
+    ! Wait for all workers
+    call MPI_BARRIER(MPI_COMM_WORLD,ierror)
 
-        ! Wait for all workers
-        call MPI_BARRIER(MPI_COMM_WORLD,ierror)
-
-        ! Master receives and merge all the calculations
-        if ( rank == MASTER ) then
-         do iproc=1,numproc-1
-          call MPI_RECV(pos(ini(iproc):fin(iproc),:),3*(fin(iproc)-ini(iproc)+1),MPI_REAL8,iproc,1,MPI_COMM_WORLD,stat,ierror)
-         end do
-
-       ! UPDATE
+    ! Master receives and merge all the calculations
+    if ( rank == MASTER ) then
         do iproc=1,numproc-1
-         call MPI_ISEND(pos(:,:), 3*N, MPI_REAL8, iproc,1,MPI_COMM_WORLD,request,ierror)
+            call MPI_RECV(pos(ini(iproc):fin(iproc),:),3*(fin(iproc)-ini(iproc)+1),&
+                    MPI_REAL8,iproc,1,MPI_COMM_WORLD,stat,ierror)
         end do
-        end if
 
-        ! Synchronize all processors
-        call MPI_BARRIER(MPI_COMM_WORLD,ierror)
-
-        ! All workers receive the positions once updated and merged
-        if (rank /= MASTER ) then
-          call MPI_RECV(pos(:,:),3*N,MPI_REAL8,MASTER,1,MPI_COMM_WORLD,stat,ierror)
-        end if
-        end_time=MPI_Wtime()
-
-        !if (rank == MASTER) then
-        !  print *,"Euler Positions Time : ",end_time-start_time," seconds"
-        !endif
-
-        end subroutine EulerPositions
-
-
-
-    ! ==== EULER VELOCITIES =================================================================
-        subroutine EulerVelocities(vel,forces,N,dimnsion,BoxSize,mass,deltat,ini,fin)
-
-        integer, intent(in)                    :: dimnsion, N
-        integer                                :: j !N=Number of part.
-        integer, dimension(0:), intent(in)     :: ini, fin
-        real*8, intent(in)                     :: BoxSize, deltat, mass
-        real*8, dimension(:,:), intent(inout)  :: vel !positions
-        real*8, dimension(:,:), intent(in)     :: forces
-        double precision                       :: start_time, lapso_time,end_time
-
-        ! Update of the velocities using Euler
-        start_time=MPI_Wtime()
-        do i=ini(rank), fin(rank)
-            vel(i,:) = vel(i,:) + deltat*forces(i,:)/mass
+        ! UPDATE
+        do iproc=1,numproc-1
+            call MPI_ISEND(pos(:,:), 3*N, MPI_REAL8, iproc,1,MPI_COMM_WORLD,request,ierror)
         end do
-        ! Send velocities from the workers to the Main
-        if (rank /= MASTER) then
-        call MPI_ISEND(vel(ini(rank):fin(rank),:),3*(fin(rank)-ini(rank)+1),MPI_REAL8,MASTER,1,MPI_COMM_WORLD,request,ierror)
-        end if
+    end if
 
-        ! Waiting all workers
-        call MPI_BARRIER(MPI_COMM_WORLD,ierror)
+    ! Synchronize all processors
+    call MPI_BARRIER(MPI_COMM_WORLD,ierror)
 
-        ! Master receive and merge
-        if ( rank == MASTER ) then
+    ! All workers receive the positions once updated and merged
+    if (rank /= MASTER ) then
+        call MPI_RECV(pos(:,:),3*N,MPI_REAL8,MASTER,1,MPI_COMM_WORLD,stat,ierror)
+    end if
+    end_time=MPI_Wtime()
+
+    !if (rank == MASTER) then
+    !  print *,"Euler Positions Time : ",end_time-start_time," seconds"
+    !endif
+
+end subroutine EulerPositions
+
+
+! ==== EULER VELOCITIES =================================================================
+subroutine EulerVelocities(vel,forces,N,dimnsion,BoxSize,mass,deltat,ini,fin)
+
+    integer, intent(in)                     ::  dimnsion, N
+    integer                                 ::  j, i
+    integer, dimension(0:), intent(in)      ::  ini, fin
+    real*8, intent(in)                      ::  BoxSize, deltat, mass
+    real*8, dimension(:,:), intent(inout)   ::  vel
+    real*8, dimension(:,:), intent(in)      ::  forces
+    double precision                        ::  start_time, lapso_time, end_time
+
+    ! Update of the velocities using Euler
+    start_time=MPI_Wtime()
+    do i=ini(rank), fin(rank)
+        vel(i,:) = vel(i,:) + deltat*forces(i,:)/mass
+    end do
+
+    ! Send velocities from the workers to the Main
+    if (rank /= MASTER) then
+        call MPI_ISEND(vel(ini(rank):fin(rank),:),3*(fin(rank)-ini(rank)+1),&
+                    MPI_REAL8,MASTER,1,MPI_COMM_WORLD,request,ierror)
+    end if
+
+    ! Waiting all workers
+    call MPI_BARRIER(MPI_COMM_WORLD,ierror)
+
+    ! Master receive and merge
+    if ( rank == MASTER ) then
          do iproc=1,numproc-1
-          call MPI_RECV(vel(ini(iproc):fin(iproc),:),3*(fin(iproc)-ini(iproc)+1),MPI_REAL8,iproc,1,MPI_COMM_WORLD,stat,ierror)
+             call MPI_RECV(vel(ini(iproc):fin(iproc),:),3*(fin(iproc)-ini(iproc)+1),&
+                        MPI_REAL8,iproc,1,MPI_COMM_WORLD,stat,ierror)
          end do
 
         ! UPDATE
         do iproc=1,numproc-1
-         call MPI_ISEND(vel(:,:), 3*N, MPI_REAL8, iproc,1,MPI_COMM_WORLD,request,ierror)
+            call MPI_ISEND(vel(:,:), 3*N, MPI_REAL8, iproc,1,MPI_COMM_WORLD,request,ierror)
         end do
-        end if
+    end if
 
-        ! Synchronize all processors
-        call MPI_BARRIER(MPI_COMM_WORLD,ierror)
+    ! Synchronize all processors
+    call MPI_BARRIER(MPI_COMM_WORLD,ierror)
 
-        ! All workers receive the velocities once updated
-        if (rank /= MASTER ) then
-          call MPI_RECV(vel(:,:),3*N,MPI_REAL8,MASTER,1,MPI_COMM_WORLD,stat,ierror)
-        end if
-        end_time=MPI_Wtime()
+    ! All workers receive the velocities once updated
+    if (rank /= MASTER ) then
+      call MPI_RECV(vel(:,:),3*N,MPI_REAL8,MASTER,1,MPI_COMM_WORLD,stat,ierror)
+    end if
+    end_time=MPI_Wtime()
 
-        !if (rank == MASTER) then
-        !  print *,"Euler Velocities Time : ",end_time-start_time," seconds"
-        !endif
+    !if (rank == MASTER) then
+    !  print *,"Euler Velocities Time : ",end_time-start_time," seconds"
+    !endif
 
-        end subroutine EulerVelocities
+end subroutine EulerVelocities
 
 
-    ! ======================================================================================
-    subroutine Refold_Positions(pos,N,dimnsion,BoxSize,ini,fin)
+! ======================================================================================
+subroutine Refold_Positions(pos,N,dimnsion,BoxSize,ini,fin)
 
-        integer, intent(in) ::dimnsion,N !N=Number of part.
-        integer                 :: i
-        real*8, intent(in) :: BoxSize
-        integer, dimension(0:), intent(in)  ::  ini, fin
-        real*8,dimension(:,:), intent(inout):: pos !positions
-        double precision:: start_time, lapso_time,end_time
+    integer, intent(in)                    ::  dimnsion, N !N=Number of part.
+    integer                                ::  i
+    real*8, intent(in)                     ::  BoxSize
+    integer, dimension(0:), intent(in)     ::  ini, fin
+    real*8,dimension(:,:), intent(inout)   ::  pos
+    double precision                       ::  start_time, lapso_time,end_time
 
-        !start calculation
-        start_time=MPI_Wtime()
-        do i=ini(rank),fin(rank)
-         pos(i,:) = pos(i,:) - BoxSize*nint(pos(i,:)/BoxSize) !periodic conditions
-        end do
+    !start calculation
+    start_time=MPI_Wtime()
+    do i=ini(rank),fin(rank)
+        pos(i,:) = pos(i,:) - BoxSize*nint(pos(i,:)/BoxSize) !periodic conditions
+    end do
 
-        !sending work of worker(i) to master
-        if (rank /= MASTER) then
-        call MPI_ISEND(pos(ini(rank):fin(rank),:),3*(fin(rank)-ini(rank)+1),MPI_REAL8,MASTER,1,MPI_COMM_WORLD,request,ierror)
-        end if
+    !sending work of worker(i) to master
+    if (rank /= MASTER) then
+        call MPI_ISEND(pos(ini(rank):fin(rank),:),3*(fin(rank)-ini(rank)+1),&
+                    MPI_REAL8,MASTER,1,MPI_COMM_WORLD,request,ierror)
+    end if
 
-        !waiting all workers
-        call MPI_BARRIER(MPI_COMM_WORLD,ierror)
+    !waiting all workers
+    call MPI_BARRIER(MPI_COMM_WORLD,ierror)
 
-        !master receive and merge
-        if ( rank == MASTER ) then
+    !master receive and merge
+    if ( rank == MASTER ) then
          do iproc=1,numproc-1
-          call MPI_RECV(pos(ini(iproc):fin(iproc),:),3*(fin(iproc)-ini(iproc)+1),MPI_REAL8,iproc,1,MPI_COMM_WORLD,stat,ierror)
+             call MPI_RECV(pos(ini(iproc):fin(iproc),:),3*(fin(iproc)-ini(iproc)+1),&
+                        MPI_REAL8,iproc,1,MPI_COMM_WORLD,stat,ierror)
          end do
 
-       !UPDATE
+        !UPDATE
         do iproc=1,numproc-1
-         call MPI_ISEND(pos(:,:), 3*N, MPI_REAL8, iproc,1,MPI_COMM_WORLD,request,ierror)
+            call MPI_ISEND(pos(:,:), 3*N, MPI_REAL8, iproc,1,MPI_COMM_WORLD,request,ierror)
         end do
-        end if
+    end if
 
-        !Sincronize all processors
-        call MPI_BARRIER(MPI_COMM_WORLD,ierror)
-        !all workers receive the coord
-        if (rank /= MASTER ) then
-          call MPI_RECV(pos(:,:),3*N,MPI_REAL8,MASTER,1,MPI_COMM_WORLD,stat,ierror)
-        end if
-        end_time=MPI_Wtime()
+    !Sincronize all processors
+    call MPI_BARRIER(MPI_COMM_WORLD,ierror)
+    !all workers receive the coord
+    if (rank /= MASTER ) then
+      call MPI_RECV(pos(:,:),3*N,MPI_REAL8,MASTER,1,MPI_COMM_WORLD,stat,ierror)
+    end if
+    end_time=MPI_Wtime()
 
-        !if (rank == MASTER) then
-        !  print *,"Refolf positions Time : ",end_time-start_time," seconds"
-        !endif
+    !if (rank == MASTER) then
+    !  print *,"Refolf positions Time : ",end_time-start_time," seconds"
+    !endif
 
-        end subroutine Refold_Positions
+end subroutine Refold_Positions
+
+! ===================================================================================
 
 end module
